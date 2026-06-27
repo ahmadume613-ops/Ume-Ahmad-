6d83bfb794273f29654bddcedd8a2b0c99cc6376
worldwide Quran <ahmadume613@gmail.com>
2026-06-27 05:25:48 +0500

    Update sanity.config.js

--- sanity.config.js
+++ sanity.config.js
@@ -1,30 +1,18 @@
-import { defineConfig } from "sanity";
-import { deskTool } from "sanity/desk";
+import { defineConfig } from 'sanity'
+import { deskTool } from 'sanity/desk'
+import { visionTool } from '@sanity/vision'
+import { schemaTypes } from './schemas'
 
 export default defineConfig({
-  name: "default",
-  title: "Worldwide Quran Academy Studio",
+  name: 'default',
+  title: 'worldwide Quran',
 
-  projectId: process.env.VITE_SANITY_PROJECT_ID || "9kbc5fi1",
-  dataset: process.env.VITE_SANITY_DATASET || "production",
+  projectId: '7z1epwzq',
+  dataset: 'production',
 
-  plugins: [deskTool()],
+  plugins: [deskTool(), visionTool()],
 
   schema: {
-    types: [
-      {
-        name: "blog",
-        title: "Blog Post",
-        type: "document",
-        fields: [
-          { name: "title", title: "Title", type: "string" },
-          { name: "slug", title: "Slug", type: "slug", options: { source: "title" } },
-          { name: "publishedAt", title: "Published At", type: "datetime" },
-          { name: "excerpt", title: "Excerpt", type: "text" },
-          { name: "body", title: "Body", type: "text" },
-          { name: "author", title: "Author", type: "string" }
-        ]
-      }
-    ]
-  }
-});
+    types: schemaTypes,
+  },
+})
